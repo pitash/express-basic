3ac3410dfe0789e62a144adb6462f4f104e295f9
pitash <pitashkumar03@gmail.com>
2024-08-13 15:56:29 +0600

    part 1

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('This is home page');
+});
+
+app.post('/', (req, res) => {
+    res.send('This is home page post request');
+});
+
+app.listen(3000, ()=> {
+    console.log('listening on port 3000');
+});
