5eb43dedc20fc09817d4ebec8087ef55658c9999
pitash <pitashkumar03@gmail.com>
2024-08-14 15:03:39 +0600

    base url

--- index.js
+++ index.js
@@ -1,74 +1,19 @@
 const express = require('express');
-// const handle = require('./handle');
 
 const app = express();
 
-app.set('view engine','ejs');
+const adminRoute = express.Router();
+adminRoute.get('/dashboard', (req, res)=>{
+    console.log(req.baseUrl);
+    res.send('We are in admin dashboard');
+});
 
-// app.enable('case sensitive routing');
+app.use('/admin', adminRoute);
 
-app.locals.title = 'My App';
-
-// app.get('/', handle);
-
-// app.get('/CaseSensitive', (req, res)=>{
-//     res.send('welcome to case sensitive routing');
-// });
-
-app.all('/', (req, res)=>{
+app.get('/', (req, res)=>{
+    console.log(req.baseUrl);
     res.send('welcome to application');
 });
-
-// Start for param
-app.param('id', (req, res, next, id)=>{
-    const user = {
-        userid: id,
-        name:'Bangladesh',
-    };
-    req.userDetails = user;
-    next();
-});
-
-app.get('/user/:id', (req, res)=>{
-    console.log(req.userDetails);
-    res.send('Welcome to id wise data');
-});
-
-// End for param
-
-// Start For Route
-
-// app.get('/', (req, res)=>{
-//     res.send('welcome to application get');
-// });
-// app.post('/', (req, res)=>{
-//     res.send('welcome to application post');
-// });
-// app.put('/', (req, res)=>{
-//     res.send('welcome to application put');
-// });
-
-app.route('/about/mission')
-    .get((req, res)=>{
-        // res.send('welcome to application get');
-        // res.render('pages/about');
-    })
-    .post((req, res)=>{
-        res.send('welcome to application post');
-    })
-    .put((req, res)=>{
-        res.send('welcome to application put');
-    });
-
-// End For Route
-
-//Start For EJS
-app.get('/ejs', (req, res)=>{
-    res.render('pages/about');
-});
-//End For EJS
-
-
 
 app.listen(3000, ()=> {
     console.log('listening on port 3000');
