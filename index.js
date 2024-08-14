1935c8147d8ae7a459eec21f7ca93245f6b684d5
pitash <pitashkumar03@gmail.com>
2024-08-14 12:01:48 +0600

    Part 3

--- index.js
+++ index.js
@@ -1,54 +1,72 @@
 const express = require('express');
+// const handle = require('./handle');
+
 const app = express();
 
-const router = express.Router();
-app.use(router);
-// app.use(express.json());
-// app.use(express.raw());
-// app.use(express.text());
-// app.use(express.urlencoded());
-// app.use(express.static(`${__dirname}/public/`));
+app.set('view engine','ejs');
 
+// app.enable('case sensitive routing');
 
-app.use(express.static(`${__dirname}/public/`, {
-    index: 'home.html',
+app.locals.title = 'My App';
+
+// app.get('/', handle);
+
+// app.get('/CaseSensitive', (req, res)=>{
+//     res.send('welcome to case sensitive routing');
+// });
+
+app.all('/', (req, res)=>{
+    res.send('welcome to application');
+});
+
+// Start for param
+app.param('id', (req, res, next, id)=>{
+    const user = {
+        userid: id,
+        name:'Bangladesh',
+    };
+    req.userDetails = user;
+    next();
+});
+
+app.get('/user/:id', (req, res)=>{
+    console.log(req.userDetails);
+    res.send('Welcome to id wise data');
+});
+
+// End for param
+
+// Start For Route
+
+// app.get('/', (req, res)=>{
+//     res.send('welcome to application get');
+// });
+// app.post('/', (req, res)=>{
+//     res.send('welcome to application post');
+// });
+// app.put('/', (req, res)=>{
+//     res.send('welcome to application put');
+// });
+
+app.route('/about/mission')
+    .get((req, res)=>{
+        // res.send('welcome to application get');
+        // res.render('pages/about');
     })
-);
+    .post((req, res)=>{
+        res.send('welcome to application post');
+    })
+    .put((req, res)=>{
+        res.send('welcome to application put');
+    });
 
-app.get('/', (req, res) => {
-    res.send('This is home page');
+// End For Route
+
+//Start For EJS
+app.get('/ejs', (req, res)=>{
+    res.render('pages/about');
 });
-
-app.post('/for-json', (req, res) => {
-    console.log(req.body);
-    res.send('This is post request for application/json and use express.json()');
-});
-
-app.post('/for-raw', (req, res) => {
-    // console.log(req.body);
-    console.log(req.body.toString());
-    res.send('This is post request for application/octet-stream and use express.raw()');
-});
-
-app.post('/for-text', (req, res) => {
-    console.log(req.body);
-    res.send('This is post request for content-type text/plain and use express.text()');
-});
-
-app.post('/for-urlencoded', (req, res) => {
-    console.log(req.body);
-    res.send('This is post request for content-type application/x-www-form-urlencoded and use express.urlencoded()');
-});
-
-app.get('/', (req, res) => {
-    // res.send('This is public access for file use this route http://localhost:3000/text/test.txt');
-    res.send('This is public access for file use if need any home route name, this route http://localhost:3000/text');
-});
-
-
-router.get('/router', (req, res) => {
-    res.send('This is Router page');
-});
+//End For EJS
 
 
 
