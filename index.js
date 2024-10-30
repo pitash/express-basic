47f265123309c214603ed69bfd97655b13469049
pitash <pitashkumar03@gmail.com>
2024-10-30 23:23:50 +0600

    File Upload with Multer Pkg In Express part-9

--- index.js
+++ index.js
@@ -1,61 +1,101 @@
 const express = require('express');
-const fs = require('fs');
+const multer = require('multer');
+const path = require('path');
+
+const UPLOADS_FOLDER = './uploads/';
+
+//define the storage
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, UPLOADS_FOLDER);
+    },
+    filename: (req, file, cb) => {
+        //Important File.pdf => important-file-3456677.pdf
+        const fileExtention = path.extname(file.originalname);
+        const filename = file.originalname.replace(fileExtention, '').toLocaleLowerCase().split(' ').join('-') + '-' + Date.now();
+        cb(null, filename + fileExtention);
+    },
+});
+
+var upload = multer({
+    // dest: UPLOADS_FOLDER,
+    storage: storage,
+    limits: {
+        fileSize: 1000000, //1MB and its bytes formate
+    },
+    fileFilter: (req, file, cb) =>{
+        // console.log(file);
+        if (file.fieldname === "avatar") {
+            if (
+              file.mimetype === "image/png" ||
+              file.mimetype === "image/jpg" ||
+              file.mimetype === "image/jpeg"
+            ) {
+              cb(null, true);
+            } else {
+              cb(new Error("Only .jpg, .png or .jpeg format allowed!"));
+            }
+          } else if (file.fieldname === "doc") {
+            if (file.mimetype === "application/pdf") {
+              cb(null, true);
+            } else {
+              cb(new Error("Only .pdf format allowed!"));
+            }
+          } else {
+            cb(new Error("There was an unknown error!"));
+          }
+        },
+    });
+
 const app = express();
 
-// app.get('/', (req, res) => {
-//     // res.send('Home');
-//     // throw new Error("There was an error");
-//     res.send(a);
+//For Singel Upload
+// app.post('/', upload.single('avatar'), (req, res) => {
+//     res.send('Hello World');
 // });
 
-// app.use((req, res, next)=> {
-//     // res.status(404).send('Request url not found');
-//     next('Request url not found');
+//For Multiple Upload
+// app.post('/', upload.array('avatar', 3), (req, res) => {
+//     res.send('Hello World');
 // });
 
-// app.use((err, req, res, next) => {
-//     if(err.message){
-//         res.status(500).send(err.message);
-//     }else {
-//         res.send('There Was an Error!');
-//     }
+//For Multiple Field Upload (multiple input field)
+// app.post('/', upload.fields([
+//     {name: 'avatar', maxCount:1 },
+//     {name: 'gallery', maxCount:2 },
+// ]), (req, res) => {
+//     res.send('Hello World');
 // });
 
 
-//For Asynchorous way
-
-app.get('/async', (req, res, next) => {
-    fs.readFile('/file-does-note-exist', (err, data) => {
-        if(err) {
-            next(err);
-        }else {
-            res.send(data);
-        }
-    });
+//For DOC and Image upload
+app.post('/', upload.fields([
+    {name: 'avatar', maxCount:1 },
+    {name: 'doc', maxCount:1 }
+]), (req, res) => {
+    res.send('Hello World');
 });
 
 
-app.get('/middleware-chaining', [
-    (req, res, next) => {
-        fs.readFile('/file-does-note-exist', 'utf-8', (err, data) => {
-            console.log(data);
-            next(err);
-        });
-    },
-    (req, res, next) => {
-        console.log(data.property);   
-    },
-]);
+//default error handler
+// app.use((err, req, res, next)=> {
+//     if(err){
+//         res.status(500).send(err.message);
+//     }else{
+//         res.send('success');
+//     }
+// });
 
+//multer err handler
 app.use((err, req, res, next) => {
-    if(res.headerSent) {
-        next("There was a problem");
-    }else{
-        if(err.message) {
+    if(err){
+        if(err instanceof multer.MulterError){
+            res.status(500).send('There Was Upload Error');
+        } else {
             res.status(500).send(err.message);
-        }else{
-            res.send('There was an error');
         }
+    }else{
+        res.send('Success');
     }
 });
 
